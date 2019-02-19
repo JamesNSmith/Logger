68821471515d1bc43426f578a702db0c0210d2e0
James Smith <jamesnsmith@hotmail.co.uk>
2019-02-19 19:29:46 +0000

    first

---
+++ db/migrate/20190219185006_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :u_id
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :nick_name
+      t.string :first_name
+      t.string :second_name
+
+      t.timestamps
+    end
+  end
+end
