1d5e6afaab602382afd1262ca37d3d6a4d239640
James Smith <jamesnsmith@hotmail.co.uk>
2019-03-05 14:27:47 +0000

    flight controller ready

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,10 @@
 class FlightsController < ApplicationController
+	def index
+		@flights = Flight.all
+		puts("flights --------------")
+		@flights.each do |flight|
+			puts(flight.id)
+			puts(flight.launch_fee)
+		end
+	end
 end
--- db/migrate/20190222235025_create_flights.rb
+++ db/migrate/20190222235025_create_flights.rb
@@ -1,17 +1,18 @@
 class CreateFlights < ActiveRecord::Migration[5.1]
   def change
     create_table :flights do |t|
-      t.belongs_to :club
+      #t.belongs_to :club
       t.string :takeoff_type
       #t.belongs_to :aircraft
 
-      t.belongs_to :club_user_p1, :references => 'club_user'
-      t.belongs_to :club_user_p2, :references => 'club_user'
+      #t.belongs_to :club_user
+      #t.belongs_to :club_user_p1, :references => 'club_user'
+      #t.belongs_to :club_user_p2, :references => 'club_user'
       #t.belongs_to :club_user_p2, :class_name => 'club_user', :foreign_key => 'club_user_p2_id'
 
-      t.date :launch
-      t.date :land
-      t.time :flight_time
+      #t.date :launch
+      #t.date :land
+      #t.time :flight_time
 
       t.decimal :launch_fee
       t.decimal :soaring_fee
